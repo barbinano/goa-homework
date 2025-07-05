c08b95b45daad20fe528f7f28a65ae39b73a57b3
barbinano <sabasaghliani18@gmail.com>
2025-07-05 22:28:04 +0400

    second commit

---
+++ day 2/classwork/classwork 2.py
@@ -0,0 +1,14 @@
+ricxvi_1 = 13
+ricxvi_2=28
+
+answer_1 = ricxvi_1 + ricxvi_2
+print(answer_1)
+
+answer_2 = ricxvi_1 - ricxvi_2
+
+print(answer_2)
+answer_3 = ricxvi_1 * ricxvi_2
+print(answer_3)
+
+answer_4 = ricxvi_1 / ricxvi_2
+print(answer_4)
