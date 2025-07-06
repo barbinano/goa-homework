a84444de0c50e7aacb28e427d99d6acdd3589bbc
barbinano <sabasaghliani18@gmail.com>
2025-07-06 17:43:12 +0400

    second commit

---
+++ day 2/homework/homework 1.py
@@ -0,0 +1,84 @@
+#დავალება 1))შექმენით ორი ცვლადი,
+#თქვენი დავალებაა ამ ორ ცვლადზე მოახდინოთ ყველა მათემატიკური მოქმედება რაც ვისწავლეთ(+ - * /)
+
+
+
+a = 23
+
+b = 10
+
+print ( a + b )
+
+print ( a - b )
+
+print ( a * b )
+
+print ( a / b )
+
+
+
+#დავალება 2)შექმენით 12 ცვლადი,4 ცვლადში შეინახე სტრინგ ტიპის მონაცემი,4 ში შეინახე ინტეჯერი და 4 ში შეინახე ფლოატი
+#დაპრინტე 12 ივე მათგანი,გამოიყენეთ ცვლადები მნიშვნელობების მაგივრად და ისე დაპრინტეთ.
+
+
+
+name = "saba"
+
+surname = "saghliani"
+
+city = "tbilisi"
+
+country = "georgia"
+
+
+
+age = 16
+
+birth_year = 2009
+
+birth_month = 4
+
+birth_day= 21
+
+
+
+height = 1.87 
+
+weight = 69.2 
+
+time_of_day = 15.22 
+
+answer_4 = a / b
+
+
+
+
+print ("name:" + name )
+
+print ("surname:" + surname )
+
+print ("city:" + city )     
+
+print ("country:" + country )
+
+
+
+print ( "age:" + str(age) )
+
+print ( "birth year:" + str(birth_year) )
+
+print ( "birth month:" + str(birth_month) )
+
+print ( "birth day:" + str(birth_day) )
+
+
+print ( "height:" + str(height) )
+
+print ( "weight:" + str(weight) )
+
+print ( "time of day:" + str(time_of_day) )  
+
+print ( "answer 4:" + str(answer_4) )
+
+
+
