b5b6be1b10a216fd3d988648d76323f84883397b
barbinano <sabasaghliani18@gmail.com>
2025-07-08 18:20:22 +0400

    second commit

--- chemtvis calke mushaoba/gartoba 0.py
+++ chemtvis calke mushaoba/gartoba 0.py
@@ -1,3 +1,5 @@
 age = input ( " how old are you  " )
 name = input ( " what is your name " ) 
-print ( "i am " + str(age) + " years old, my name is " + name + ".")
+print ( "i am " + age + " years old, my name is " + name + ".")
+
+
