a6c5b4ddbe07c96000c7bc8b16751e4076b8013f
barbinano <sabasaghliani18@gmail.com>
2025-09-13 23:11:32 +0400

    second commit

---
+++ goa course/day 22/classwork/classwork.py
@@ -0,0 +1,34 @@
+names = ["saba", "lia", "beqa", "gurami", "hamleti", "lamzira", "geno", "vitia", "Iani", "andria"]
+
+
+
+
+
+i=0
+while i < 10:
+    print(names[i])
+    
+    i=i+1
+
+
+
+
+
+
+
+
+
+    
+vegetables = ["კარტოფილი", "სტაფილო", "ბადრიჯანი", "კომბოსტო", "კიტრი", "პამიდორი"]
+
+
+vegetables[0] = "viri"        
+vegetables[3] = "tagvit"      
+vegetables[5] = "bokveri"     
+
+
+i=0
+while i < 6:
+    print(vegetables[i])
+    
+    i=i+1
