be431003f63b940c974392fd753e5ad9fed0ea2a
barbinano <sabasaghliani18@gmail.com>
2025-10-25 18:21:18 +0400

    second commit

---
+++ goa course/day 31/classwork/classwork.py
@@ -0,0 +1,11 @@
+#1)შექმენით სია --> ["ina" , "givi" , "nika" , "daviti" , "ia" , "lizi"] , თქვენი დავალებაა რომ პირველი 2 ელემენტი ჩაანაცვლოთ შემდეგი სიით --> ["irina" , "milana" , "kira", "mate"] //////////////// და ასევე სიის ბოლო ორი ელემენტი შეანაცვლე შემდეგი სიით --> ["gia" , "emzari" , "xvicha"] ამის შემდეგ დაპრინტეთ საბოლოო სია
+
+
+name = ["ina", "givi", "nika", "daviti", "ia", "lizi"]
+
+name[0:2] = ["irina", "milana", "kira", "mate"]
+
+name[6:8] = ["gia", "emzari", "xvicha"]
+
+print(name)
+
