26f4ecaf2f634091871fe112bdc97c2ebc579859
barbinano <sabasaghliani18@gmail.com>
2025-12-24 15:41:34 +0400

    second commit

---
+++ goa course/day 51/classwork/classwork.py
@@ -0,0 +1,29 @@
+# num=[ 21 , 24 , 33 , 45, 52 , 6 , 7 ]
+
+# numbers=[   ]
+
+# for i in num:
+#     if i % 2 == 0 and num.index(i) % 2 != 0:
+#         numbers.append(i)
+
+
+# print(numbers)
+
+
+
+# numbers_1 = 5
+
+# numbers_2=[]
+
+# for i in range(6, 51 , 2):
+#     numbers_2.append(i)
+
+
+# print(numbers_2)
+
+
+
+
+
+
+
