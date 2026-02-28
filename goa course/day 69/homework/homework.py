1cddd0d795a8c0342cc5e9e8f2091d735c98845d
barbinano <sabasaghliani18@gmail.com>
2026-02-28 15:19:50 +0400

    second commit

---
+++ goa course/day 69/homework/homework.py
@@ -0,0 +1,33 @@
+# def solution(string):
+#     return string[::-1]
+ 
+
+
+
+
+# def positive_sum(arr):
+#     total = 0
+#     for i in arr:
+#         if i > 0:
+#             total += i
+#     return total
+
+
+
+
+# def repeat_str(repeat, string):
+#         return repeat * string
+    
+    
+
+# def square_sum(numbers):
+#     total = 0
+#     for i in numbers:
+#         squared = i * i
+#         total += squared
+#     return total
+
+
+
+# def string_to_number(s):
+#     return int(s)
