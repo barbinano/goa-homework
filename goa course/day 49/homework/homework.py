fa11d53bf5b3167ed8211d032b4d619dbc142463
barbinano <sabasaghliani18@gmail.com>
2026-03-22 00:21:19 +0400

    second commit

---
+++ goa course/day 49/homework/homework.py
@@ -0,0 +1,51 @@
+# 1
+# def make_negative(number):
+#     if number > 0:
+#         return number * -1
+#     elif number <= 0:
+#         return number
+
+
+
+# 2
+
+# def opposite(number):
+#     if number == 0:
+#         return 0
+#     else:
+#         return number * -1
+
+
+
+# 3
+
+
+# def  find_smallest_int(arr):
+#     smallest = arr[0]   
+    
+#     for i in arr:
+#         if i < smallest:
+#             smallest = i
+    
+#     return smallest
+
+
+# 4
+
+# def count_sheeps(sheep):
+#     count = 0
+    
+#     for i in sheep:
+#         if i == True:
+#             count += 1
+    
+#     return count
+
+
+# 5
+
+# def boolean_to_string(b): 
+#     if b == True:
+#         return "True"
+#     elif b == False:
+#         return "False"
