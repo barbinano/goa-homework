38caaf6a5320a4b84919a00a2abe936cac5cadd2
barbinano <sabasaghliani18@gmail.com>
2026-03-25 23:56:08 +0400

    second commit

--- goa course/chemtvis calke mushaoba/gartoba 0.py
+++ goa course/chemtvis calke mushaoba/gartoba 0.py
@@ -1,4 +1,21 @@
+import time
 
+password = "12342"
+
+print("Cracking Password....")
+
+for i in range(100000000):
+    guess = str(i).zfill(5)
+
+   
+
+    if guess == password:
+
+        print("Password Found")
+
+        print("Password is:", guess)
+        
+        break
 
 
 
